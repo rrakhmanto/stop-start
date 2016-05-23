c111f6061214a1b031f0df30eeb4c78023240ba6
mike1372 <michaelkempster@bigpond.com>
2016-05-23 16:51:12 +1000

    Added my profile credentials for testing as I don't want to mess with Base2 ASGs that may be important...

--- stop-start.js
+++ stop-start.js
@@ -2,6 +2,9 @@
 console.log('Loading function');
 var AWS = require('aws-sdk');
 AWS.config.region = 'ap-southeast-2';
+var credentials = new AWS.SharedIniFileCredentials({profile: 'm.kempster'});
+AWS.config.credentials = credentials;
+
 var autoscaling = new AWS.AutoScaling(); 
 
 autoscaling.describeAutoScalingGroups({}, function(err, data) {
