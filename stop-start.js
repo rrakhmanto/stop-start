f6254774ac3b919e8cbe29bcbd0f979fc0f05207
mike1372 <michaelkempster@bigpond.com>
2016-05-24 09:48:10 +1000

    Added code to start as well as stop.

--- stop-start.js
+++ stop-start.js
@@ -7,22 +7,47 @@ AWS.config.credentials = credentials;
 
 var autoscaling = new AWS.AutoScaling(); 
 
+var stopStart = stop;
+
+const ZERO = 0;
+const ONE = 1;
+
 // Terminate the ASG instances by setting the group size to zero
 autoscaling.describeAutoScalingGroups({}, function(err, data) {
   if (err) {
     console.log(err, err.stack);
   } else {
     console.log(data.AutoScalingGroups);
-
-    data.AutoScalingGroups.forEach(adjustGroupSize, 0);
+    if (stopStart === stop) {
+      data.AutoScalingGroups.forEach(downsizeGroupSize);
+    } else if (stopStart === start) {
+      data.AutoScalingGroups.forEach(upsizeGroupSize);
+    } else {
+      console.log('Error: please choose eithet start or stop as the action to perform');
+    }
   }
 });
 
-function adjustGroupSize(group, number) {
+function downsizeGroupSize(group, number) {
   var updateParams = {
     AutoScalingGroupName: group.AutoScalingGroupName,
-    MaxSize: number,
-    MinSize: number
+    MaxSize: ZERO,
+    MinSize: ZERO
+  }
+  autoscaling.updateAutoScalingGroup(updateParams, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+function upsizeGroupSize(group, number) {
+  var updateParams = {
+    AutoScalingGroupName: group.AutoScalingGroupName,
+    MaxSize: ONE,
+    MinSize: ONE
   }
   autoscaling.updateAutoScalingGroup(updateParams, function(err, data) {
     if (err) {
