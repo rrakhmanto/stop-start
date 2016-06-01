b30b449a041074e9764049b1fa138d3d4b604fef
mike1372 <michaelkempster@bigpond.com>
2016-06-01 17:51:39 +1000

    New script to manually invoke the function, e.g. to start out of hours.

---
+++ manual-running/invoke.js
@@ -0,0 +1,52 @@
+'use strict';
+console.log('Running...');
+var AWS = require('aws-sdk');
+AWS.config.region = 'us-east-1';
+var lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
+var functionName = 'stop-start-stop-start';
+
+// Get all function details
+// Note: does not yet handle pagination
+function listFunctions() {
+  console.log('Getting functions...')
+  lambda.listFunctions({}, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      // console.log(data.Functions);
+      checkFunctionName(data.Functions);
+    }
+  });
+}
+
+// Search the list of functions for the correct name
+function checkFunctionName(functions) {
+  console.log('Checking for function existence...')
+  for (var i = 0; i < functions.length; i++) {
+    if (functions[i].FunctionName === functionName) {
+      invokeFunction(functions[i].FunctionName);
+      return;
+    }
+  }
+  console.log('Stop-Start function does not appear to exist, aborting...');
+  process.exit(1);
+}
+
+// Invoke the function with the specified event configuration parameters
+function invokeFunction(name) {
+  var eventConfig = '{ "stopStart": "stop", "reportOnly": false, "environment": "prod", "tableName": "stop-start", "region": "ap-southeast-2" }';
+  var params = {
+    FunctionName: name,
+    InvocationType: 'RequestResponse',
+    Payload: eventConfig
+  };
+  lambda.invoke(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+listFunctions();
